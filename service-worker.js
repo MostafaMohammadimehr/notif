16f7be87898fc3e7a7d78be39c1034d52a4eedf9
MostafaMohammadimehr <m.mohammadimehr1390@gmail.com>
2025-06-28 14:02:17 +0330

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,8 @@
+if (Notification.permission !== 'granted' && Notification.permission !== 'denied') {
+  Notification.requestPermission().then(permission => {
+    console.log('Notification permission:', permission);
+  });
+}
 self.addEventListener('install', event => {
   self.skipWaiting();
 });
